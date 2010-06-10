b3ff4dba1e549bd09c4c932091d63d44f5739262
azumakuniyuki <azumakuniyuki@gmail.com>
2010-06-10 16:17:39 +0900

    * Code refactoring.

--- s-vs-y.pl
+++ s-vs-y.pl
@@ -5,32 +5,79 @@ use Test::More 'no_plan';
 
 my $V = 'perl 5.8.8';
 my $E = '<postmaster@example.jp>';
+my $M = 'Some::Module::Name::Of::Perl';
 
-sub ss { my $x = shift(); my $y = shift(); $x =~ s{\D}{}g; $y =~ s{[<>]}{}g; $y =~ s{x}{X}g; return($x.$y); }
-sub yy { my $x = shift(); my $y = shift(); $x =~ y{[0-9]}{}cd; $y =~ y{[<>]}{}d; $y =~ y{x}{X}; return($x.$y); }
+sub s1 { my $x = shift(); $x =~ s{\D}{}g; return($x); }
+sub s2 { my $x = shift(); $x =~ s{[<>]}{}g; return($x); }
+sub s3 { my $x = shift(); $x =~ s{::}{/}g; $x .= '.pm'; return($x); }
 
-is( ss($V,$E), '588postmaster@eXample.jp' );
-is( yy($V,$E), '588postmaster@eXample.jp' );
+sub y1 { my $x = shift(); $x =~ y{0-9}{}cd; return($x); }
+sub y2 { my $x = shift(); $x =~ y{<>}{}d; return($x); }
+sub y3 { my $x = shift(); $x =~ y{:}{/}s; $x .= '.pm'; return($x); }
+
+is( s1($V), '588' );
+is( s2($E), 'postmaster@example.jp' );
+is( s3($M), 'Some/Module/Name/Of/Perl.pm' );
+
+is( y1($V), '588' );
+is( y2($E), 'postmaster@example.jp' );
+is( y3($M), 'Some/Module/Name/Of/Perl.pm' );
 
 cmpthese(500000, { 
-	's///g' => sub { &ss($V,$E) }, 
-	'y///d' => sub { &yy($V,$E) }, 
+	's{\D}{}g' => sub { &s1($V) }, 
+	'y{0-9}{}cd' => sub { &y1($V) }, 
+});
+print "\n";
+cmpthese(500000, { 
+	's{[<>]}{}g' => sub { &s2($E) }, 
+	'y{<>}{}d' => sub { &y2($E) }, 
+});
+print "\n";
+cmpthese(500000, { 
+	's{::}{/}g' => sub { &s3($M) }, 
+	'y{:}{/}s' => sub { &y3($M) }, 
 });
 
 __END__
 
 * PowerBookG4/Perl 5.8.8
-          Rate s///g y///d
-s///g 137363/s    --  -56%
-y///d 314465/s  129%    --
+               Rate   s{\D}{}g y{0-9}{}cd
+s{\D}{}g   263158/s         --       -59%
+y{0-9}{}cd 649351/s       147%         --
+
+               Rate s{[<>]}{}g   y{<>}{}d
+s{[<>]}{}g 384615/s         --       -38%
+y{<>}{}d   625000/s        63%         --
+
+              Rate s{::}{/}g  y{:}{/}s
+s{::}{/}g 240385/s        --      -54%
+y{:}{/}s  526316/s      119%        --
+
 
 * PowerBookG4/Perl 5.10.0
-          Rate s///g y///d
-s///g  83056/s    --  -66%
-y///d 247525/s  198%    --
+               Rate   s{\D}{}g y{0-9}{}cd
+s{\D}{}g   157233/s         --       -70%
+y{0-9}{}cd 526316/s       235%         --
+
+               Rate s{[<>]}{}g   y{<>}{}d
+s{[<>]}{}g 263158/s         --       -51%
+y{<>}{}d   537634/s       104%         --
+
+              Rate s{::}{/}g  y{:}{/}s
+s{::}{/}g 142045/s        --      -67%
+y{:}{/}s  431034/s      203%        --
+
 
 * PowerBookG4/Perl 5.12.0
-          Rate s///g y///d
-s///g  65531/s    --  -72%
-y///d 230415/s  252%    --
+               Rate   s{\D}{}g y{0-9}{}cd
+s{\D}{}g   107296/s         --       -79%
+y{0-9}{}cd 515464/s       380%         --
+
+               Rate s{[<>]}{}g   y{<>}{}d
+s{[<>]}{}g 239234/s         --       -52%
+y{<>}{}d   500000/s       109%         --
+
+              Rate s{::}{/}g  y{:}{/}s
+s{::}{/}g 123762/s        --      -62%
+y{:}{/}s  324675/s      162%        --
 
