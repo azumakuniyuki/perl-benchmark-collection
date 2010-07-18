802f627b39f40624776da79295026d6bd0f2072a
azumakuniyuki <azumakuniyuki@gmail.com>
2010-07-19 02:57:25 +0900

    * Add code: map { $_ = ... }

--- perl-best-practice/6.12.convert-list-by-map-vs-for.pl
+++ perl-best-practice/6.12.convert-list-by-map-vs-for.pl
@@ -18,29 +18,42 @@ sub convertbymapfunc
 	@$x = map { int sqrt($_) } @$x;
 }
 
+sub convertbymapitself
+{
+	my $x = shift();
+	map { $_ = int sqrt($_) } @$x;
+}
+
 my @X = @Data; convertbyforloop(\@X);
 my @Y = @Data; convertbymapfunc(\@Y);
+my @Z = @Data; convertbymapitself(\@Z);
 is( $X[-1], 9 ); @X = @Data;
 is( $Y[-1], 9 ); @Y = @Data;
+is( $Z[-1], 9 ); @Z = @Data;
 
 cmpthese(10000, { 
-	'for()' => sub { &convertbyforloop(\@X) }, 
-	'map{}' => sub { &convertbymapfunc(\@Y) }, 
+	'for my $x(..)' => sub { &convertbyforloop(\@X) }, 
+	'@x = map{...}' => sub { &convertbymapfunc(\@Y) }, 
+	'map{$_ = ...}' => sub { &convertbymapitself(\@Z) }, 
 });
 
 __END__
 
 * PowerBookG4/perl 5.8.8
-         Rate map{} for()
-map{}  8197/s    --  -50%
-for() 16393/s  100%    --
+                 Rate @x = map{...} map{$_ = ...} for my $x(..)
+@x = map{...}  7937/s            --           -9%          -52%
+map{$_ = ...}  8696/s           10%            --          -48%
+for my $x(..) 16667/s          110%           92%            --
 
 * PowerBookG4/perl 5.10.0
-         Rate map{} for()
-map{}  5618/s    --  -54%
-for() 12195/s  117%    --
+                 Rate @x = map{...} map{$_ = ...} for my $x(..)
+@x = map{...}  5650/s            --          -10%          -53%
+map{$_ = ...}  6250/s           11%            --          -47%
+for my $x(..) 11905/s          111%           90%            --
 
 * PowerBookG4/perl 5.12.0
-         Rate map{} for()
-map{}  6410/s    --  -53%
-for() 13514/s  111%    --
+                 Rate @x = map{...} map{$_ = ...} for my $x(..)
+@x = map{...}  6329/s            --           -4%          -53%
+map{$_ = ...}  6579/s            4%            --          -51%
+for my $x(..) 13514/s          114%          105%            --
+
