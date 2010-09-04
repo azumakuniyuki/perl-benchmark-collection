544523e309be033dd7029dc8647edeaabc65951a
azumakuniyuki <azumakuniyuki@gmail.com>
2010-09-05 03:06:06 +0900

    * Remove 'List::MoreUtils::apply'

--- array/create-list-by-map-vs-for.pl
+++ array/create-list-by-map-vs-for.pl
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 use Benchmark ':all';
 use Test::More 'no_plan';
-use List::Util;
 
 my $Data = [ 0..99 ];
 sub createbyforloop
@@ -19,41 +18,33 @@ sub createbymapfunc
 {
 	my $x = shift();
 	my $y = [ map { int sqrt($_) } @$x ];
-}
-
-sub createbyluapply
-{
-	my $x = shift();
-	my $y = [ List::Util::apply { int sqrt($_) } @$x ];
+	return $y;
 }
 
 my $f = createbyforloop($Data);
 my $m = createbymapfunc($Data);
-my $a = createbyluapply($Data);
 is( $f->[-1], 9 );
 is( $m->[-1], 9 );
-is( $a->[-1], 9 );
 
 cmpthese(10000, { 
 	'for()' => sub { &createbyforloop($Data) }, 
-	'map{}' => sub { &createbymapfunc($Data) }, 
-	'apply' => sub { &createbyluapply($Data) }, 
+	'map {...}' => sub { &createbymapfunc($Data) }, 
 });
 
 __END__
 
 * PowerBookG4/perl 5.8.8
-         Rate for() map{}
-for()  9091/s    --  -12%
-map{} 10309/s   13%    --
+             Rate     for() map {...}
+for()      9091/s        --      -10%
+map {...} 10101/s       11%        --
 
 * PowerBookG4/perl 5.10.0
-        Rate for() map{}
-for() 6579/s    --  -12%
-map{} 7519/s   14%    --
+            Rate     for() map {...}
+for()     6494/s        --      -14%
+map {...} 7519/s       16%        --
 
 * PowerBookG4/perl 5.12.0
-        Rate for() map{}
-for() 7752/s    --   -5%
-map{} 8130/s    5%    --
+            Rate     for() map {...}
+for()     7752/s        --       -5%
+map {...} 8130/s        5%        --
 
