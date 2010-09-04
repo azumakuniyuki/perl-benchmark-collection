84276dfc818e117c980f2c51107d6cfe900f145b
azumakuniyuki <azumakuniyuki@gmail.com>
2010-09-05 01:24:57 +0900

    * splice() vs. pop()

---
+++ array/splice-vs-pop.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+# pop() vs. splice()
+use strict;
+use warnings;
+use Benchmark ':all';
+use Test::More 'no_plan';
+
+my @Data = ( 0..99 );
+sub popthearray { my @a = @Data; my $x = pop(@a); $x += pop(@a); return $x; }
+sub splicearray { my @a = @Data; my $x = splice(@a,-1); $x += splice(@a,-1); return $x; }
+
+is( popthearray(), 197 );
+is( splicearray(), 197 );
+
+cmpthese(1000000, { 
+	'pop' => sub { popthearray() }, 
+	'splice' => sub { splicearray() }, 
+});
+
+__END__
+
+* PowerBookG4/perl 5.8.8
+          Rate splice    pop
+splice 46275/s     --    -1%
+pop    46707/s     1%     --
+
+* PowerBookG4/perl 5.10.0
+          Rate splice    pop
+splice 34400/s     --    -3%
+pop    35348/s     3%     --
+
+* PowerBookG4/perl 5.12.0
+          Rate splice    pop
+splice 39062/s     --    -1%
+pop    39417/s     1%     --
+
