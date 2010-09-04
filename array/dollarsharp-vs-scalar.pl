ddf2df98d38e050d088a2cf642bfdbac373b3806
azumakuniyuki <azumakuniyuki@gmail.com>
2010-09-05 03:41:18 +0900

    * Update: add the code $scalar = @array;

--- array/dollarsharp-vs-scalar.pl
+++ array/dollarsharp-vs-scalar.pl
@@ -7,32 +7,39 @@ use Test::More 'no_plan';
 
 my @A = ();
 my @B = ();
+my @C = ();
 sub dollsharp { push( @A, shift() ); return( $#A + 1 ); }
 sub usescalar { push( @B, shift() ); return( scalar(@B) ); }
+sub usereturn { push( @C, shift() ); return @C; }
 
 is( dollsharp(0), 1 );
 is( usescalar(0), 1 );
+is( usereturn(0), 1 );
 
-my @C = ( 1 .. 100 );
+my @Z = ( 1 .. 100 );
 cmpthese( 10000, { 
-		'($#A + 1)' => sub{ &dollsharp($_) for @C; },
-		'scalar @B' => sub{ &usescalar($_) for @C; },
+		'($#A + 1)' => sub{ &dollsharp($_) for @Z; },
+		'scalar @B' => sub{ &usescalar($_) for @Z; },
+		'return @C' => sub{ &usereturn($_) for @Z; },
 	});
 
 __END__
 
 * PowerBookG5/perl 5.8.8
-            Rate ($#A + 1) scalar @B
-($#A + 1) 5376/s        --      -13%
-scalar @B 6211/s       16%        --
+            Rate ($#A + 1) scalar @B return @C
+($#A + 1) 5263/s        --      -14%      -23%
+scalar @B 6098/s       16%        --      -10%
+return @C 6803/s       29%       12%        --
 
 * PowerBookG4/perl 5.10.0
-            Rate ($#A + 1) scalar @B
-($#A + 1) 4082/s        --      -26%
-scalar @B 5495/s       35%        --
+            Rate ($#A + 1) scalar @B return @C
+($#A + 1) 4098/s        --      -24%      -32%
+scalar @B 5376/s       31%        --      -11%
+return @C 6024/s       47%       12%        --
 
 * PowerBookG4/perl 5.12.0
-            Rate ($#A + 1) scalar @B
-($#A + 1) 4545/s        --      -13%
-scalar @B 5208/s       15%        --
-
+            Rate ($#A + 1) scalar @B return @C
+($#A + 1) 4566/s        --      -13%      -21%
+scalar @B 5236/s       15%        --      -10%
+return @C 5814/s       27%       11%        --
+	    
