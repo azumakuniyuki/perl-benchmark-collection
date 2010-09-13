5dadd002d87e3263ae75f043ba902f97c1e4b316
azumakuniyuki <azumakuniyuki@gmail.com>
2010-09-13 20:08:23 +0900

    * Add the result of File::Slurp and Rename .

--- module/slurp-competition.pl
+++ module/slurp-competition.pl
@@ -6,29 +6,35 @@ use Benchmark ':all';
 use Test::More 'no_plan';
 use Perl6::Slurp;
 use Path::Class::File;
+use File::Slurp;
 
 my $file = '/etc/passwd';
 my $path = new Path::Class::File($file);
 
 sub perl6slurp { my $x = Perl6::Slurp::slurp($file); return $x; }
 sub pathclassf { my $x = $path->slurp(); return $x; }
+sub fileslurp  { my $x = File::Slurp::slurp($file); return $x; }
 
-my $size = [ length perl6slurp(), length pathclassf() ];
+my $size = [ length perl6slurp(), length pathclassf(), length fileslurp() ];
 ok( $size->[0], $file.' = '.$size->[0] );
 ok( $size->[1], $file.' = '.$size->[1] );
+ok( $size->[2], $file.' = '.$size->[2] );
 is( $size->[0], $size->[1] );
+is( $size->[1], $size->[2] );
 
 cmpthese(10000, { 
 	'Perl6::Slurp->slurp' => sub { perl6slurp(); },
 	'Path::Class::File->slurp' => sub { pathclassf(); },
+	'File::Slurp->slurp' => sub { fileslurp(); },
 });
 
 __END__
 
 * PowerBookG4/perl 5.10.0
-                           Rate Path::Class::File->slurp     Perl6::Slurp->slurp
-Path::Class::File->slurp 2304/s                       --                    -59%
-Perl6::Slurp->slurp      5556/s                     141%                      --
+                           Rate Path::Class::File->slurp Perl6::Slurp->slurp File::Slurp->slurp
+Path::Class::File->slurp 2132/s                       --                -60%               -77%
+Perl6::Slurp->slurp      5348/s                     151%                  --               -43%
+File::Slurp->slurp       9434/s                     342%                 76%                 --
 
 * PowerBookG4/perl 5.12.0
                            Rate Path::Class::File->slurp     Perl6::Slurp->slurp
