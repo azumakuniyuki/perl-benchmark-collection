034117c20b4acd2da332ce24aa746c2d41fac7fb
azumakuniyuki <azumakuniyuki@gmail.com>
2013-01-08 19:30:01 +0900

    Update for Issue #1, Thanks to vsespb.

--- operator/s-vs-y.pl
+++ operator/s-vs-y.pl
@@ -9,11 +9,11 @@ my $M = 'Some::Module::Name::Of::Perl';
 
 sub s1 { my $x = shift(); $x =~ s{\D}{}g; return($x); }
 sub s2 { my $x = shift(); $x =~ s{[<>]}{}g; return($x); }
-sub s3 { my $x = shift(); $x =~ s{::}{/}g; $x .= '.pm'; return($x); }
+sub s3 { my $x = shift(); $x =~ s{:}{/}g; $x =~ y{/}{}s; $x .= '.pm'; return($x); }
 
 sub y1 { my $x = shift(); $x =~ y{0-9}{}cd; return($x); }
 sub y2 { my $x = shift(); $x =~ y{<>}{}d; return($x); }
-sub y3 { my $x = shift(); $x =~ y{:}{/}s; $x .= '.pm'; return($x); }
+sub y3 { my $x = shift(); $x =~ y{:}{/};  $x =~ y{/}{}s; $x .= '.pm'; return($x); }
 
 is( s1($V), '588' );
 is( s2($E), 'postmaster@example.jp' );
@@ -23,75 +23,61 @@ is( y1($V), '588' );
 is( y2($E), 'postmaster@example.jp' );
 is( y3($M), 'Some/Module/Name/Of/Perl.pm' );
 
-cmpthese(500000, { 
+cmpthese(1200000, { 
 	's{\D}{}g' => sub { &s1($V) }, 
 	'y{0-9}{}cd' => sub { &y1($V) }, 
 });
 print "\n";
-cmpthese(500000, { 
+cmpthese(900000, { 
 	's{[<>]}{}g' => sub { &s2($E) }, 
 	'y{<>}{}d' => sub { &y2($E) }, 
 });
 print "\n";
-cmpthese(500000, { 
-	's{::}{/}g' => sub { &s3($M) }, 
-	'y{:}{/}s' => sub { &y3($M) }, 
+cmpthese(900000, { 
+	's{:}{/}g' => sub { &s3($M) }, 
+	'y{:}{/}' => sub { &y3($M) }, 
 });
 
 __END__
 
-* PowerBookG4/Perl 5.8.8
+* MacBook Air/Mac OS X 10.7.5/Perl 5.14.2
+                Rate   s{\D}{}g y{0-9}{}cd
+s{\D}{}g    459770/s         --       -79%
+y{0-9}{}cd 2222222/s       383%         --
+
+                Rate s{[<>]}{}g   y{<>}{}d
+s{[<>]}{}g  841121/s         --       -58%
+y{<>}{}d   2000000/s       138%         --
+
+              Rate s{:}{/}g  y{:}{/}
+s{:}{/}g  290323/s       --     -80%
+y{:}{/}  1475410/s     408%       --
+
+
+* OpenBSD 5.2/Perl 5.12.2
                Rate   s{\D}{}g y{0-9}{}cd
-s{\D}{}g   263158/s         --       -59%
-y{0-9}{}cd 649351/s       147%         --
+s{\D}{}g   145808/s         --       -83%
+y{0-9}{}cd 851064/s       484%         --
 
                Rate s{[<>]}{}g   y{<>}{}d
-s{[<>]}{}g 384615/s         --       -38%
-y{<>}{}d   625000/s        63%         --
+s{[<>]}{}g 368852/s         --       -55%
+y{<>}{}d   825688/s       124%         --
 
-              Rate s{::}{/}g  y{:}{/}s
-s{::}{/}g 240385/s        --      -54%
-y{:}{/}s  526316/s      119%        --
-
-
-* PowerBookG4/Perl 5.10.0
-               Rate   s{\D}{}g y{0-9}{}cd
-s{\D}{}g   157233/s         --       -70%
-y{0-9}{}cd 526316/s       235%         --
-
-               Rate s{[<>]}{}g   y{<>}{}d
-s{[<>]}{}g 263158/s         --       -51%
-y{<>}{}d   537634/s       104%         --
-
-              Rate s{::}{/}g  y{:}{/}s
-s{::}{/}g 142045/s        --      -67%
-y{:}{/}s  431034/s      203%        --
+             Rate s{:}{/}g  y{:}{/}
+s{:}{/}g 106132/s       --     -68%
+y{:}{/}  334572/s     215%       --
 
 
-* PowerBookG4/Perl 5.12.0
-               Rate   s{\D}{}g y{0-9}{}cd
-s{\D}{}g   107296/s         --       -79%
-y{0-9}{}cd 515464/s       380%         --
+* Ubuntu 10.04.4 LTS/perl 5.12.4
+                Rate   s{\D}{}g y{0-9}{}cd
+s{\D}{}g    262009/s         --       -79%
+y{0-9}{}cd 1263158/s       382%         --
 
-               Rate s{[<>]}{}g   y{<>}{}d
-s{[<>]}{}g 239234/s         --       -52%
-y{<>}{}d   500000/s       109%         --
+                Rate s{[<>]}{}g   y{<>}{}d
+s{[<>]}{}g  514286/s         --       -59%
+y{<>}{}d   1250000/s       143%         --
 
-              Rate s{::}{/}g  y{:}{/}s
-s{::}{/}g 123762/s        --      -62%
-y{:}{/}s  324675/s      162%        --
-
-
-* Ubuntu 8.04 LTS/perl 5.10.1
-               Rate   s{\D}{}g y{0-9}{}cd
-s{\D}{}g   306748/s         --       -68%
-y{0-9}{}cd 961538/s       213%         --
-
-               Rate s{[<>]}{}g   y{<>}{}d
-s{[<>]}{}g 438596/s         --       -45%
-y{<>}{}d   793651/s        81%         --
-
-              Rate s{::}{/}g  y{:}{/}s
-s{::}{/}g 246305/s        --      -66%
-y{:}{/}s  714286/s      190%        --
+             Rate s{:}{/}g  y{:}{/}
+s{:}{/}g 161290/s       --     -77%
+y{:}{/}  697674/s     333%       --
 
