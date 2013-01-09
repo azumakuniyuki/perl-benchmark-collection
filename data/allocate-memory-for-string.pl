986e314934cfcf57de88cbadaa6237960b44e7bd
azumakuniyuki <azumakuniyuki@gmail.com>
2013-01-09 16:29:27 +0900

    Update benchmark codes and results

--- data/allocate-memory-for-string.pl
+++ data/allocate-memory-for-string.pl
@@ -4,42 +4,37 @@ use strict;
 use warnings;
 use Benchmark ':all';
 
-my $S = '.' x (1 << 8);
-cmpthese(1000000, { 
-	'Allocation1' => sub { my $v = q{ } x (1<<9); $v = $S; },
-	'Allocation2' => sub { my $v = q{ } x (1<<8); $v = $S; },
-	'Allocation3' => sub { my $v = q{ } x (1<<5); $v = $S; },
-	'Expansion'   => sub { my $v = undef(); $v = $S; },
+my $STRING = '.' x ( 1 << 16 );
+
+cmpthese(200000, { 
+	'Allocation1' => sub { my $v = ' ' x ( 1 << 18 ); $v = $STRING; },
+	'Allocation2' => sub { my $v = ' ' x ( 1 << 16 ); $v = $STRING; },
+	'Allocation3' => sub { my $v = ' ' x ( 1 <<  8 ); $v = $STRING; },
+	'Allocation4' => sub { my $v = ' ' x ( 1 <<  4 ); $v = $STRING; },
+	'Allocation5' => sub { my $v = ' ' x ( 1 <<  1 ); $v = $STRING; },
+	'Allocation6' => sub { my $v = ' ' x ( 1 <<  0 ); $v = $STRING; },
+	'Expansion'   => sub { my $v = undef;             $v = $STRING; },
 });
 
 __END__
 
-* PowerBookG4/perl 5.8.8
-                 Rate Allocation1 Allocation2 Allocation3   Expansion
-Allocation1  729927/s          --        -23%        -45%        -67%
-Allocation2  943396/s         29%          --        -29%        -58%
-Allocation3 1333333/s         83%         41%          --        -40%
-Expansion   2222222/s        204%        136%         67%          --
+* Mac OS X 10.7.5/Perl 5.14.2
+                Rate Allocation1 Allocation2 Allocation3 Allocation4 Allocation6 Allocation5 Expansion
+Allocation1  25510/s          --        -75%        -91%        -91%        -91%        -91%      -91%
+Allocation2 102564/s        302%          --        -64%        -65%        -65%        -65%      -66%
+Allocation3 285714/s       1020%        179%          --         -1%         -1%         -1%       -4%
+Allocation4 289855/s       1036%        183%          1%          --         -0%         -0%       -3%
+Allocation6 289855/s       1036%        183%          1%          0%          --         -0%       -3%
+Allocation5 289855/s       1036%        183%          1%          0%          0%          --       -3%
+Expansion   298507/s       1070%        191%          4%          3%          3%          3%        --
 
-* PowerBookG4/perl 5.10.0
-                 Rate Allocation1 Allocation2 Allocation3   Expansion
-Allocation1  578035/s          --        -23%        -39%        -66%
-Allocation2  746269/s         29%          --        -22%        -56%
-Allocation3  952381/s         65%         28%          --        -44%
-Expansion   1694915/s        193%        127%         78%          --
-
-* PowerBookG4/perl 5.12.0
-                 Rate Allocation2 Allocation1 Allocation3   Expansion
-Allocation2  990099/s          --         -0%         -8%        -42%
-Allocation1  990099/s          0%          --         -8%        -42%
-Allocation3 1075269/s          9%          9%          --        -37%
-Expansion   1694915/s         71%         71%         58%          --
-
-* Ubuntu 8.04 LTS/perl 5.10.1
-            (warning: too few iterations for a reliable count)
-                 Rate Allocation1 Allocation2 Allocation3   Expansion
-Allocation1  847458/s          --        -24%        -39%        -69%
-Allocation2 1111111/s         31%          --        -20%        -59%
-Allocation3 1388889/s         64%         25%          --        -49%
-Expansion   2702703/s        219%        143%         95%          --
+* OpenBSD 5.2/Perl 5.12.2
+               Rate Allocation1 Allocation2 Allocation3 Allocation6 Allocation4 Allocation5 Expansion
+Allocation1  3549/s          --        -85%        -94%        -95%        -95%        -95%      -95%
+Allocation2 23474/s        561%          --        -63%        -64%        -64%        -65%      -65%
+Allocation3 64103/s       1706%        173%          --         -2%         -2%         -3%       -6%
+Allocation6 65147/s       1736%        178%          2%          --          0%         -2%       -4%
+Allocation4 65147/s       1736%        178%          2%          0%          --         -2%       -4%
+Allocation5 66225/s       1766%        182%          3%          2%          2%          --       -3%
+Expansion   68027/s       1817%        190%          6%          4%          4%          3%        --
 
