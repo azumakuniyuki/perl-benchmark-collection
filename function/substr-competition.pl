6575b2d2ed28dc9863e7109db6417c85c9b89155
azumakuniyuki <azumakuniyuki@gmail.com>
2013-01-09 18:43:31 +0900

    Add results at Mac OS X 10.7.5, OpenBSD 5.2

--- function/substr-competition.pl
+++ function/substr-competition.pl
@@ -7,13 +7,25 @@ use Test::More 'no_plan';
 
 my $Data = '68b329da9893e34099c7d8ad5cb9c940';
 my $Text = '0' x 16;
-sub substrlvalue { my $x = shift(); substr( $x, 8, 16 ) = $Text; return $x; }
-sub substr4argvs { my $x = shift(); substr( $x, 8, 16, $Text );  return $x; }
+
+sub substrlvalue
+{
+	my $x = shift; 
+	substr( $x, 8, 16 ) = $Text;
+	return $x;
+}
+
+sub substr4argvs
+{ 
+	my $x = shift;
+	substr( $x, 8, 16, $Text );
+	return $x;
+}
 
 like( substrlvalue($Data), qr{\A.{8}$Text.{8}\z} );
 like( substr4argvs($Data), qr{\A.{8}$Text.{8}\z} );
 
-cmpthese(400000, { 
+cmpthese(1400000, { 
 	'lvalue' => sub { substrlvalue($Data) }, 
 	'4argvs' => sub { substr4argvs($Data) }, 
 });
@@ -41,3 +53,12 @@ lvalue 322581/s     --   -39%
 lvalue  625000/s     --   -41%
 4argvs 1052632/s    68%     --
 
+* Mac OS X 10.7.5/Perl 5.14.2
+            Rate lvalue 4argvs
+lvalue  800000/s     --   -66%
+4argvs 2333333/s   192%     --
+
+* OpenBSD 5.2/Perl 5.12.2
+           Rate lvalue 4argvs
+lvalue 516605/s     --   -39%
+4argvs 848485/s    64%     --
