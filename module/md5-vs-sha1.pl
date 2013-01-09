eb9b9b8af9be210d0575237f2f36ab4ad9b1cffa
azumakuniyuki <azumakuniyuki@gmail.com>
2013-01-09 18:57:12 +0900

    Add results at Mac OS X 10.7.5, OpenBSD 5.2

--- module/md5-vs-sha1.pl
+++ module/md5-vs-sha1.pl
@@ -15,7 +15,7 @@ my $str = '794-uguisu-heiankyo';
 ok( md5($str), md5($str) );
 ok( sha($str), sha($str) );
 
-cmpthese(300000, { 
+cmpthese(600000, { 
 	'Digest::MD5' => sub { &md5($str); },
 	"Digest::SHA" => sub { &sha($str); },
 });
@@ -37,4 +37,14 @@ Digest::MD5 315789/s        441%          --
                 Rate Digest::SHA Digest::MD5
 Digest::SHA 120000/s          --        -84%
 Digest::MD5 769231/s        541%          --
+
+* Mac OS X 10.7.5/Perl 5.14.2
+                 Rate Digest::SHA Digest::MD5
+Digest::SHA  191083/s          --        -85%
+Digest::MD5 1250000/s        554%          --
+
+* OpenBSD 5.2/Perl 5.12.2
+                Rate Digest::SHA Digest::MD5
+Digest::SHA  77320/s          --        -83%
+Digest::MD5 461538/s        497%          --
 
