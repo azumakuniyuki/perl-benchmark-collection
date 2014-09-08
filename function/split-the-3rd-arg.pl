85af2198d668e1c6134fd5e6d34c7aa2a608adb4
azumakuniyuki <azumakuniyuki@gmail.com>
2014-09-08 19:51:12 +0900

    Add more benchmark sub routines

--- function/split-the-3rd-arg.pl
+++ function/split-the-3rd-arg.pl
@@ -5,31 +5,48 @@ use warnings;
 use Benchmark ':all';
 use Test::More 'no_plan';
 
-my $L = [ 0..9 ];
+my $L = [ 0..99 ];
 my $S = join( ':', @$L );
 
-sub use3rd { 
-    my @x = split( ':', $S, 5 );
+sub use3rd75 { 
+    my @x = split( ':', $S, 75 );
 	return scalar @x;
 }
 
-sub twoarg {
+sub use3rd50 { 
+    my @x = split( ':', $S, 50 );
+	return scalar @x;
+}
+
+sub use3rd25 { 
+    my @x = split( ':', $S, 25 );
+	return scalar @x;
+}
+
+sub twoargvs {
 	my @x = split( ':', $S );
 	return scalar @x;
 }
 
-is( twoarg(), 10 );
-is( use3rd(), 5 );
+is( twoargvs(), 100 );
+is( use3rd25(),  25 );
+is( use3rd50(),  50 );
+is( use3rd75(),  75 );
 
 cmpthese( 900000, { 
-	'split(":",@x)' => sub { twoarg() }, 
-	'split(":",@x,5)' => sub { use3rd() }, 
+	'split(":",@x)'     => sub { twoargvs() }, 
+	'split(":",@x,25)'  => sub { use3rd25() }, 
+	'split(":",@x,50)'  => sub { use3rd50() }, 
+	'split(":",@x,75)'  => sub { use3rd75() }, 
 });
 
 __END__
 
 * Mac OS X 10.9.4/Perl 5.14.2
-                    Rate   split(":",@x) split(":",@x,5)
-split(":",@x)   255682/s              --            -43%
-split(":",@x,5) 445545/s             74%              --
+
+                     Rate split(":",@x) split(":",@x,75) split(":",@x,50) split(":",@x,25)
+split(":",@x)     30293/s            --             -25%             -51%             -74%
+split(":",@x,75)  40614/s           34%               --             -34%             -66%
+split(":",@x,50)  61983/s          105%              53%               --             -48%
+split(":",@x,25) 118577/s          291%             192%              91%               --
 
