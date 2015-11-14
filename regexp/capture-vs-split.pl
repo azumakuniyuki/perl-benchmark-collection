a7a9f6ef0dd755d514b61f54cae69201fa3bbe63
azumakuniyuki <azumakuniyuki@gmail.com>
2015-11-14 11:06:21 +0900

    $v =~ m/(.+)/ vs. split() for getting strings

---
+++ regexp/capture-vs-split.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+# Regular Expression: (?:a|b|c) v.s. (a|b|c)
+use strict;
+use warnings;
+use Benchmark ':all';
+use Test::More 'no_plan';
+
+my $X = 'From: MAILER-DAEMON <mailer-daemon@example.jp>';
+
+sub docapture { 
+    my $x = shift;
+    my $y = '';
+    if( $x =~ m/\A([-0-9A-Za-z]+?)[:][ ]*.+\z/ ) {
+        $y = lc $1;
+    }
+    return $y;
+}
+
+sub usesplit {
+    my $x = shift;
+    my $y = '';
+    if( $x =~ m/\A[-0-9A-Za-z]+?[:][ ]*.+\z/ ) {
+        my @v = split( ':', $x, 2 );
+        $y = lc $v[0];
+    }
+    return $y;
+}
+
+is( docapture($X), 'from' );
+is( usesplit($X),  'from' );
+
+cmpthese(300000, { 
+    'Capture' => sub { &docapture($X) }, 
+    'split()' => sub { &usesplit($X) }, 
+});
+
+__END__
+
+Mac OS X 10.9.5/Perl 5.20.1
+ok 1
+ok 2
+            Rate split() Capture
+split() 357143/s      --    -45%
+Capture 652174/s     83%      --
